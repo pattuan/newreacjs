9935a987bc02d6cdc804a9f249031dc1f57ddad0
tuan <tonytuanpat2601@gmail.com>
2024-04-17 23:20:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Stack } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./page/layout";
+import Home from "./page/home";
+import Order from "./page/order";
+import Login from "./page/login";
+import Register from "./page/register";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter basename="/newreacjs">
+        <Routes>
+            <Route exact path="/Layout" element={<Layout />}/>
+                
+            
+            <Route path="/home" element={<Home />} />
+            <Route path="/order" element={<Order />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+          </Routes>
+        </BrowserRouter>
+        
     </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./page/layout";
+import Home from "./page/home";
+import Order from "./page/order";
+import Login from "./page/login";
+import Register from "./page/register";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+     {/* <Routes>
+  //     <Route path="/" element={<Layout />}>
+  //       <Route index element={<Home />} />
+  //       <Route path="/order" element={<Order />} />
+  //     </Route>
+  //     <Route path="/login" element={<Login />} />
+  //     <Route path="/register" element={<Register />} />
+  //   </Routes> */}
     <App />
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
